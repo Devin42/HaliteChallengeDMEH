a2b16a05964ec75e5ef8724762ac1d2be06fcc6a
Ethan Hellman <student@ethan-hellman.dalton.org>
2017-11-11 14:27:12 -0500

    5 ships or new planet strat

--- MyBot.java
+++ MyBot.java
@@ -1,6 +1,9 @@
+
 import hlt.*;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MyBot {
 
@@ -18,32 +21,47 @@ public class MyBot {
 
         final ArrayList<Move> moveList = new ArrayList<>();
         for (;;) {
+            boolean allColonized = false;
+            
             moveList.clear();
             networking.updateMap(gameMap);
 
-            for (final Ship ship : gameMap.getMyPlayer().getShips().values()) {
+            for (final Ship ship :  gameMap.getMyPlayer().getShips().values()) {
                 if (ship.getDockingStatus() != Ship.DockingStatus.Undocked) {
                     continue;
                 }
-
-                for (final Planet planet : gameMap.getAllPlanets().values()) {
-                    if (planet.isOwned()) {
-                        continue;
+                
+                Map<Double, Entity> entities_by_distance = gameMap.nearbyEntitiesByDistance(ship);
+                Planet nearest_planet = null;
+                for(Entity entity : entities_by_distance.values()) {
+                    if(entity instanceof Planet) {
+                        nearest_planet = (Planet) entity;
+                        if(nearest_planet.isOwned() && nearest_planet.getOwner() != gameMap.getMyPlayer().getId()){
+                            continue;
+                        }
+                        else if(nearest_planet.getOwner() == gameMap.getMyPlayer().getId() && (nearest_planet.getDockedShips().size() > 5 || nearest_planet.getDockingSpots()-nearest_planet.getDockedShips().size() == 0)) {
+                            continue;
+                        }
+                        else {
+                            break;
+                        }
                     }
-
-                    if (ship.canDock(planet)) {
-                        moveList.add(new DockMove(ship, planet));
-                        break;
-                    }
-
-                    final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED/2);
-                    if (newThrustMove != null) {
-                        moveList.add(newThrustMove);
-                    }
-
-                    break;
                 }
+
+                if (ship.canDock(nearest_planet)) {
+                    moveList.add(new DockMove(ship, nearest_planet));
+                    continue;
+                }
+
+
+                final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, nearest_planet, Constants.MAX_SPEED);
+                if (newThrustMove != null) {
+                    moveList.add(newThrustMove);
+                }
+
+                continue;
             }
+            
             Networking.sendMoves(moveList);
         }
     }
